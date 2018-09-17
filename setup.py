78a023958a97796c0d8e97879ed183224e4b1757
Thiago de Arruda <tpadilha84@gmail.com>
2018-09-17 18:52:52 -0300

    Include contrib directory as installed extensions

--- setup.py
+++ setup.py
@@ -12,6 +12,11 @@ setup(
     description='i3 extension runtime',
     python_requires='>=3.5',
     py_modules=['i3hub'],
+    data_files=[('share/i3hub/extensions', [
+        'contrib/status_wrapper.py',
+        'contrib/binding.py',
+        'contrib/keyboard_layout_switcher.py'
+        ])],
     author='Thiago de Arruda',
     author_email='tpadilha84@gmail.com',
     url=REPO,
